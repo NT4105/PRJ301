66e1d71cf498a19625713cca1c33c310fb70dbd4
NT4105 <tunguyen04105@gmail.com>
2025-05-18 22:26:50 +0700

    add servlet files

--- Lab01_Servlet_SE193851/src/java/ServletPkg/BookServlet.java
+++ Lab01_Servlet_SE193851/src/java/ServletPkg/BookServlet.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package ServletPkg;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -15,43 +16,69 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author ACER
  */
-@WebServlet(name = "BookServlet", urlPatterns = {"/BookServlet"})
+@WebServlet(name = "BookServlet", urlPatterns = { "/BookServlet" })
 public class BookServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        String id = request.getParameter("txtId");
+        String title = request.getParameter("txtTitle");
+        String author = request.getParameter("txtPublisher");
+        double price = Double.parseDouble(request.getParameter("txtPrice"));
+        int quantity = Integer.parseInt(request.getParameter("txtQuantity"));
+        double subTotal = price * quantity;
+        PrintWriter out = response.getWriter();
+        try {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet BookServlet</title>");            
+            out.println("<title>AddBook</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet BookServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Book Details</h1>");
+            StringBuilder strBookDetail = new StringBuilder();
+            strBookDetail.append("<table border='1'>");
+            strBookDetail.append("<thead>");
+            strBookDetail.append("<tr>");
+            strBookDetail.append("<th>BookId</th>");
+            strBookDetail.append("<th>Title</th>");
+            strBookDetail.append("<th>Publisher</th>");
+            strBookDetail.append("<th>Price</th>");
+            strBookDetail.append("<th>Quantity</th>");
+            strBookDetail.append("<th>SubTotal</th>");
+            strBookDetail.append("</tr>");
+            strBookDetail.append("</thead>");
+            strBookDetail.append("<tbody>");
+            strBookDetail.append("<tr>");
+            strBookDetail.append("<td>" + id + "</td>");
+            strBookDetail.append("<td>" + title + "</td>");
+            strBookDetail.append("<td>" + author + "</td>");
+            strBookDetail.append("<td>" + price + "</td>");
+            strBookDetail.append("<td>" + quantity + "</td>");
+            strBookDetail.append("<td>" + subTotal + "</td>");
+            strBookDetail.append("</tr>");
+            strBookDetail.append("</tbody>");
+            strBookDetail.append("</table>");
+            out.println(strBookDetail.toString());
+            out.println("<a href='CreateBook.html'>Back</a><br>");
             out.println("</body>");
             out.println("</html>");
+        } finally {
+            out.close();
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -62,10 +89,10 @@ public class BookServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
--- Lab01_Servlet_SE193851/src/java/ServletPkg/LoginServlet.java
+++ Lab01_Servlet_SE193851/src/java/ServletPkg/LoginServlet.java
@@ -16,43 +16,44 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author ACER
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+@WebServlet(name = "LoginServlet", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+        String userName = request.getParameter("txtUserName");
+        String password = request.getParameter("txtPassword");
+        PrintWriter out = response.getWriter();
+        try {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("<title>Login</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            if (userName.equals("admin") && password.equals("admin")) {
+                out.println("<h3>User logged in : " + userName + "</h3>");
+                out.println("<a href='CreateBook.html'>Click here to create book</a>");
+            } else {
+                response.sendRedirect("LoginFailed.html");
+            }
             out.println("</body>");
             out.println("</html>");
+        } finally {
+            out.close();
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -63,10 +64,10 @@ public class LoginServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
