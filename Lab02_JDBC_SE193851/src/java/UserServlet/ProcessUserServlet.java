47f22bdbc932b8ffdc9628e2d554f014e2247b69
NT4105 <tunguyen04105@gmail.com>
2025-05-21 20:16:13 +0700

    feat: fix sth

--- Lab02_JDBC_SE193851/src/java/UserServlet/ProcessUserServlet.java
+++ Lab02_JDBC_SE193851/src/java/UserServlet/ProcessUserServlet.java
@@ -1,9 +1,5 @@
 package UserServlet;
 
-import DBUtils.UserDAO;
-import DBUtils.User;
-import java.util.List;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -17,64 +13,31 @@ public class ProcessUserServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String userName, password, lastName;
-        boolean isAdmin = true;
-        String action;
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        action = request.getParameter("action").toLowerCase();
+        String action = request.getParameter("action").toLowerCase();
+
         try {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>User Management</title>");
+            out.println("<title>Manage Users</title>");
             out.println("</head>");
             out.println("<body>");
-            userName = request.getParameter("txtUserName");
-            UserDAO userDAO = new UserDAO();
             if (action.equals("view")) {
-                out.println("<h1>View User List" + "</h1>");
-                List<User> userList = userDAO.getUserList();
-                for (User user : userList) {
-                    out.println(user + "</br>");
-                }
+                response.sendRedirect("ManageUserServlet?action=view");
+            } else if (action.equals("add")) {
+                response.sendRedirect("AddUser.html");
+            } else if (action.equals("update")) {
+                response.sendRedirect("UpdateUser.html");
             } else if (action.equals("delete")) {
-                out.println("<h1>Users Management Delete user" + "</h1>");
-                if (userDAO.deleteUser(userName) == true) {
-                    out.println("User has been deleted successfully" + "</br>");
-                } else {
-                    out.println("<h3>Something went wrong!</h3></br>");
-                }
-            } else {
-                password = request.getParameter("txtPassword");
-                lastName = request.getParameter("txtLastName");
-                String admin = request.getParameter("checkisAdmin");
-                if (admin == null) {
-                    isAdmin = false;
-                }
-                User user = new User(userName, password, lastName, isAdmin);
-                if (action.equals("add")) {
-                    out.println("<h1>Users Management - Add new user" + "</h1>");
-                    if (userDAO.addUser(user) == true) {
-                        out.println("User has been added successfully</br>");
-                    } else {
-                        out.println("<h3>Something went wrong!</h3></br>");
-                    }
-                } else if (action.equals("update")) {
-                    out.println("<h1>Users Management - Update user" + "</h1>");
-                    if (userDAO.updateUser(user) == true) {
-                        out.println("User has been updated successfully</br>");
-                    } else {
-                        out.println("<h3>Something went wrong!</h3></br>");
-                    }
-                }
+                response.sendRedirect("DeleteUser.html");
             }
-            out.println("a href='Login.html'>Back to the login</a>");
             out.println("</body>");
             out.println("</html>");
         } catch (Exception ex) {
-            out.println("Error: " + ex.getMessage());
+            out.println("");
         }
     }
 
